0b3dbfa20bac412c4b06fc5d21d1cf25e4fb47db
PabloBarTo <pablo02barto@correo.ugr.es>
2023-10-20 12:20:11 +0200

    Resuelto issue #12
    He creado una nueva clase que es equipoReal que nos servirá para saber a qué equipo pertenece cada jugador y tambien para luego ver si el jugador se enfrenta a un equipo mejor o peor que el suyo closes #12

---
+++ src/equipoReal.ts
@@ -0,0 +1,17 @@
+export class equipoReal{
+    nombre:string;
+    puesto:number;
+
+    constructor(nombre:string, puesto:number){
+        this.nombre = nombre;
+        this.puesto = puesto;
+    }
+
+    getNombre():string{
+        return this.nombre;
+    }
+
+    getPuesto():number{ 
+        return this.puesto;
+    }
+}
