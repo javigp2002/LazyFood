b1197895e76dadab59842927aafe810d6d8dd4c7
PabloBarTo <pablo02barto@correo.ugr.es>
2023-10-24 17:23:57 +0200

    Agregación de un nuevo atributo en la clase equipoReal
    Se ha añadido el atributo siguienteRival a la clase equipoReal mediante el cual se podrá saber contra que equipo juega el jugador, ya que desde el jugador podemos acceder al equipo al que pertenece  y desde ese equipo podremos ver contra quien juega closes #11

--- src/equipoReal.ts
+++ src/equipoReal.ts
@@ -1,10 +1,12 @@
 export class equipoReal{
     nombre: string;
     puesto: number;
+    siguiente_rival: equipoReal;
 
-    constructor(nombre:string, puesto:number){
+    constructor(nombre:string, puesto:number, siguiente_rival:equipoReal){
         this.nombre = nombre;
         this.puesto = puesto;
+        this.siguiente_rival = siguiente_rival;
     }
 
     getNombre():string{
@@ -13,5 +15,13 @@ export class equipoReal{
 
     getPuesto():number{ 
         return this.puesto;
+    }
+
+    getSiguienteRival():equipoReal{
+        return this.siguiente_rival;
+    }
+
+    setSiguienteRival(siguiente_rival:equipoReal):void{
+        this.siguiente_rival = siguiente_rival;
     }
 }
