0d0eafe15f8b0bca8610f51faa54e6e5389c8d3c
PabloBarTo <pablo02barto@correo.ugr.es>
2023-10-27 09:08:12 +0200

    Equidad en el formato de todas las variables y declaración de las variables en el constructor

--- src/jugador.ts
+++ src/jugador.ts
@@ -1,24 +1,20 @@
 import { equipoReal } from "./equipo_real";
 
 export class Jugador {
-    private _nombre: string;
-    private _puntuacionPorJornada: number[];
-    private _valor_por_jornada: number[];
-    private equipo_al_que_pertenece: equipoReal;
 
-    constructor(nombre: string, puntuacionPorJornada: number[], valor_por_jornada: number[], equipo_al_que_pertenece: equipoReal) {
-        this._nombre = nombre;
-        this._puntuacionPorJornada = puntuacionPorJornada;
-        this._valor_por_jornada = valor_por_jornada;
-        this.equipo_al_que_pertenece = equipo_al_que_pertenece;
-    }
+
+    constructor(
+        private nombre: string, 
+        private puntuacionPorJornada: number[], 
+        private valor_por_jornada: number[], 
+        private equipo_al_que_pertenece: equipoReal) {}
 
     getPuntuacionPorJornada(): number[] {
-        return this._puntuacionPorJornada;
+        return this.puntuacionPorJornada;
     }
 
     getValorPorJornada(): number[] {
-        return this._valor_por_jornada;
+        return this.valor_por_jornada;
     }
 
 }
