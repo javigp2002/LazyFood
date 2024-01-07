d1144aaf82f7c689ca2e72fb88ec609f8ffd0c5e
JaviGP2002 <javigp2002@gmail.com>
2024-01-08 00:37:38 +0100

    :sparkles: Init: crea bd temporal con kv closes #75
    Se va a crear una base de datos con KV donde podremos almacenar tuplas clave valor con los equipos y los jugadores de los mismos para poder devolverlos, crear, modificar o eliminar en una bd temporal

---
+++ model/bd.ts
@@ -0,0 +1,73 @@
+
+const kv = await Deno.openKv();
+const nombreEquiposKv = "equipos";
+
+export  async function createBd() {
+
+    const key = [nombreEquiposKv, "equipo1"]
+    const value = {
+        "jugadores": [
+            {
+                "nombre": "Callejon",
+                "puntuacionPorJornada": [
+                    10,
+                    10,
+                    10,
+                    10
+                ],
+                "valor_por_jornada": [
+                    10000000,
+                    10000000,
+                    10000000,
+                    10000000
+                ],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            },
+            {
+                "nombre": "Uzuni",
+                "puntuacionPorJornada": [
+                    5,
+                    5,
+                    5,
+                    5
+                ],
+                "valor_por_jornada": [
+                    10000000,
+                    10000000,
+                    10000000,
+                    10000000
+                ],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            },
+            {
+                "nombre": "Gavi",
+                "puntuacionPorJornada": [
+                    1,
+                    1,
+                    1,
+                    1
+                ],
+                "valor_por_jornada": [
+                    40000000,
+                    40000000,
+                    40000000,
+                    40000000
+                ],
+                "equipo_al_que_pertenece": {
+                    "nombre": "barcelona",
+                    "puesto": 3
+                }
+            }
+        ]    
+    };
+    
+    await kv.set(key, value);
+
+    return kv.get(key);
+};
