97ef2282a01a493b612d625c1df5f489d1cf5645
JaviGP2002 <javigp2002@gmail.com>
2024-01-09 13:11:10 +0100

    :white_check_mark: Add testing for bd, closes #89
    Testeamos que la base de datos esté aplicando la lógica de negocio necesaria cada vex que se introduce, cambia o crea (como kv está en fase de pruebas se tienen q eliminar los que se crean puesto que no se puede reiniciar la bbdd)

--- test/bd_test.ts
+++ test/bd_test.ts
@@ -1,76 +1,147 @@
-import { describe,it,beforeAll } from "https://deno.land/std@0.204.0/testing/bdd.ts";
+import { describe,it,beforeAll, afterAll } from "https://deno.land/std@0.204.0/testing/bdd.ts";
 import { assert, assertInstanceOf, assertEquals } from "https://deno.land/std@0.205.0/assert/mod.ts";
-import { createBd, deleteEquipo, deleteJugador, getEquipos, getJugador, getOptimo, postEquipo, postJugador, putEquipo, putJugador, IEquipo, crearCalendario } from "../model/bd.ts";
+import { MyDb, crearCalendario, IEquipo } from "../model/bd.ts";
 import { isErrorStatus } from "https://deno.land/std@0.200.0/http/http_status.ts";
+import { assertIsError } from "https://deno.land/std@0.205.0/assert/assert_is_error.ts";
 
 
-describe ("M3 - Bd", () => {
+describe ("M3 - Bd", async () => {
+    let db: MyDb;
+    
 
-    it ("M3.1 - Creacion Base de datos", async () => {
-        const res = await createBd();
-        assert(res != null);
+    beforeAll(async () => {
+        const kv = await createbdForTesting();
+        db = new MyDb(kv, crearCalendario());
+    });
+
+
+    it ("M3.1 - Creacion Base de datos", () => {
+        assert(db != null);
     });
 
     it ("M3.2 - Coger óptimo", async () => {
-        const jugador = (await getOptimo("equipo1"));
+        const result = await db.getOptimo("equipo1");
 
-        assertEquals(jugador.getNombre(), "Gavi");
+        assertEquals(result.getNombre(), "Gavi");
     });
 
     it ("M3.3 - Coger Jugador", async () => {
-        const jugador = (await getJugador("Gavi"));
+        const jugador = await db.getJugador("Gavi");
 
         assertEquals(jugador, "Gavi");
     });
 
     it ("M3.4 - Crear Jugador", async () => {
-        const jugador = (await postJugador(JSON.parse('{"nombre": "Uzuki", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}')));
-
+        const jugador = (await db.postJugador(JSON.parse('{"nombre": "Moradona", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}')));
+        await db.deleteJugador("Moradona");
         assert(jugador.ok);
+
+    });
+
+    it ("M3.4.1 - Crear Jugador, creado", async () => {
+        const jugador = (await db.postJugador(JSON.parse('{"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}')));
+        assert(!jugador.ok);
+
+
     });
 
     it ("M3.5 - Crear Equipo", async () => {
-        const equipos = (await postEquipo(JSON.parse('{"nombre": "equipoPrueba", "jugadores": [{"nombre": "Uzuki", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}, {"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]}')));
-
+        const equipos = (await db.postEquipo(JSON.parse('{"nombre": "galacticos", "jugadores": [{"nombre": "Uzuki", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}, {"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]}')));
+        await db.deleteEquipo("galacticos");
         assert(equipos.ok);
+
     });
 
+    it ("M3.5.1 - Crear Equipo, Creado...", async () => {
+        const equipos = (await db.postEquipo(JSON.parse('{"nombre": "equipo1", "jugadores": [{"nombre": "Uzuki", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}, {"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]}')));
+
+        assert(!equipos.ok);
+    });
 
     it ("M3.6 - Modificar jugador", async () => {
-        const jugador = (await putJugador("Uzuki", JSON.parse('{"valor_por_jornada": [20000000,20000000,10000000,10000000]}')));
-        assert(jugador.ok == true);
+        const jugador = (await db.putJugador("Carlos", JSON.parse('{"valor_por_jornada": [20000000,20000000,10000000,10000000]}')));
+        assert(jugador.ok);
     });
 
     it ("M3.7 - Modificar Equipo", async () => {
-        const equipo = (await putEquipo("equipoPrueba", JSON.parse('{"jugadores": [{"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]}')));
+        const equipo = (await db.putEquipo("equipo1", JSON.parse('{"jugadores": [{"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]}')));
         assert(equipo.ok);
     });
 
-    it ("M3.8 - Comprobar que las interfaces tengan unos parámetros establecidos", () => {
-        
-        const calendar = crearCalendario();
-        const equipo: IEquipo = {
-            nombre: "equipoPrueba",
-            jugadores: [{
-                nombre: "Carlos",
-                puntuacionPorJornada: [5,5,5,5],
-                valor_por_jornada: [10000000,10000000,10000000,10000000],
-                equipo_al_que_pertenece: {
-                    nombre: "granada",
-                    puesto: 20
-                }
-            }],
-            calendario: calendar
-        }
-
-        assertEquals(equipo.nombre, "equipoPrueba");
-        assertEquals(equipo.jugadores[0].nombre, "Carlos");
-        assertEquals(equipo.jugadores[0].puntuacionPorJornada, [5,5,5,5]);
-        assertEquals(equipo.jugadores[0].valor_por_jornada, [10000000,10000000,10000000,10000000]);
-        assertEquals(equipo.jugadores[0].equipo_al_que_pertenece.nombre, "granada");
-        assertEquals(equipo.jugadores[0].equipo_al_que_pertenece.puesto, 20);
-
+    afterAll(async () => {
+        await db.clear(); 
     });
 
 
 });
+
+
+async function createbdForTesting(): Promise<Deno.Kv>{
+    const kv = await Deno.openKv();
+
+    const key = ["equipos", "equipo1"];
+    const value = {
+        "jugadores": [
+            {
+                "nombre": "Callejon",
+                "puntuacionPorJornada": [10,10,10,10],
+                "valor_por_jornada": [10000000,10000000,10000000,10000000],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            },
+            {
+                "nombre": "Uzuni",
+                "puntuacionPorJornada": [5,5,5,5],
+                "valor_por_jornada": [10000000,10000000,10000000,10000000],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            },
+            {
+                "nombre": "Gavi",
+                "puntuacionPorJornada": [1,1,1,1],
+                "valor_por_jornada": [40000000,40000000,40000000,40000000],
+                "equipo_al_que_pertenece": {
+                    "nombre": "barcelona",
+                    "puesto": 3
+                }
+            }
+        ]    
+    };
+    
+    await kv.set(key, value);
+
+    const key2 = ["equipos", "equipo2"]
+    const value2 = {
+        "jugadores": [
+            {
+                "nombre": "Neva",
+                "puntuacionPorJornada": [10,10,10,10],
+                "valor_por_jornada": [10000000,10000000,10000000,10000000],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            },
+            {
+                "nombre": "Carlos",
+                "puntuacionPorJornada": [5,5,5,5],
+                "valor_por_jornada": [10000000,10000000,10000000,10000000],
+                "equipo_al_que_pertenece": {
+                    "nombre": "granada",
+                    "puesto": 20
+                }
+            }
+
+        ]    
+    };
+
+    await kv.set(key2, value2);
+ 
+    await kv.set(["jugadores", "Gavi"], {"jugadores": [{"nombre": "Carlos", "puntuacionPorJornada": [5,5,5,5],"valor_por_jornada": [10000000,10000000,10000000,10000000],"equipo_al_que_pertenece": {"nombre": "granada","puesto": 20}}]});
+
+    return kv;
+}
